312302c3417f465d70660520fa23a42070781cef
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 17:25:24 +0400

    IsVAlidLastName method added UserValidations class

--- Blog_management/Blog_management/AplicationLogic/Validations/UserValidations.cs
+++ Blog_management/Blog_management/AplicationLogic/Validations/UserValidations.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Blog_management.AplicationLogic.Validations
@@ -19,6 +20,18 @@ namespace Blog_management.AplicationLogic.Validations
 
             Console.WriteLine("The name you entered is incorrect, make sure the name contains only letters, the first letter is capitalized, and the length is greater than 3 and less than 30.");
 
+            return false;
+        }
+        public static bool IsValidLastName(string lastName)
+        {
+            Regex regex = new Regex(@"^(?=[A-Z]{1})([A-Za-z]{3,30})$");
+
+            if (regex.IsMatch(lastName))
+            {
+                return true;
+            }
+
+            Console.WriteLine("The last name you entered is incorrect, make sure that the last name contains only letters, the first letter is capitalized, and the length is greater than 3 and less than 30.");
             return false;
         }
     }
