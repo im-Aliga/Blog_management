ec3eda6b501b599a445c8ea501770f4d4e19d701
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 15:02:39 +0400

    BlogComments class hass been completed

--- Blog_management/Blog_management/DataBase/Model/BlogComments.cs
+++ Blog_management/Blog_management/DataBase/Model/BlogComments.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Model
 {
-    internal class BlogComments
+    public class BlogComments : Entity<int>
     {
+        public Blog WhichBlog { get; set; }
+
+        public User From { get; set; }
+
+        public string CommentContent { get; set; }
+
+        public BlogComments(Blog whichBlog, User form, string commentContent, int? id = null)
+        {
+            WhichBlog = whichBlog;
+            From = form;
+            CommentContent = commentContent;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else
+            {
+                Id = UserReposity.IdCounter;
+            }
+
+
+        }
+
+        public string GetCommentInfo()
+        {
+            return RegistrationTime + " " + From.FirstName + " " + From.LastName + " " + CommentContent;
+        }
+
+
     }
 }
