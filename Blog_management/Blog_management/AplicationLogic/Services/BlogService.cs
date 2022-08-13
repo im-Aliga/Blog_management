a3b7f2a6c9fbd416b56e606e5bf45e934cebcd17
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 16:38:39 +0400

    ShowBlogsWithComment

--- Blog_management/Blog_management/AplicationLogic/Services/BlogService.cs
+++ Blog_management/Blog_management/AplicationLogic/Services/BlogService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model;
+using Blog_management.DataBase.Reposity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace Blog_management.AplicationLogic.Services
 {
-    internal class BlogService
+    public class BlogService
     {
+        public static BlogReposity blogRepo = new BlogReposity();
+
+        public static CommentReposity commentRepo = new CommentReposity();
+        public static void ShowBlogsWithComment()
+        {
+            BlogReposity blogRepo = new BlogReposity();
+            CommentReposity commentRepo = new CommentReposity();
+            List<Blog> blogs = blogRepo.GetAll();
+            int counter = 1;
+            foreach (Blog blog in blogs)
+            {
+                if (blog.BlogStatus == BlogStatus.Approve)
+                {
+                    Console.WriteLine(blog.BlogInfo());
+
+                    foreach (BlogComments comment in commentRepo.GetAll(c => c.WhichBlog == blog))
+                    {
+
+                        Console.WriteLine($"{counter}{"."}  { comment.GetCommentInfo()}");
+                        counter++;
+                        Console.WriteLine(" ");
+                    }
+                }
+
+
+            }
+
+        }
+
     }
 }
