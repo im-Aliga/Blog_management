bf1df3f119368cfbce333d51a77487e7857933ab
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 16:06:02 +0400

    InboxReposity class has been complated

--- Blog_management/Blog_management/DataBase/Reposity/InboxReposity.cs
+++ Blog_management/Blog_management/DataBase/Reposity/InboxReposity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Reposity
 {
-    internal class InboxReposity
+    public class InboxReposity : Common.Reposity<Inbox, int>
     {
+
     }
 }
