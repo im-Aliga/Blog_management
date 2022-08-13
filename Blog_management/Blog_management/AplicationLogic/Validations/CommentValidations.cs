366924e5600ba5286f7bcf79784f34d719531b42
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 17:20:05 +0400

    IsValidContent method added CommentValidations class

--- Blog_management/Blog_management/AplicationLogic/Validations/CommentValidations.cs
+++ Blog_management/Blog_management/AplicationLogic/Validations/CommentValidations.cs
@@ -8,5 +8,14 @@ namespace Blog_management.AplicationLogic.Validations
 {
     internal class CommentValidations
     {
+        public static bool IsValidContent(string content)
+        {
+            if (content.Length >= 10 & content.Length <= 35)
+            {
+                return true;
+            }
+            Console.WriteLine("Length sould be between 10 and 35");
+            return false;
+        }
     }
 }
