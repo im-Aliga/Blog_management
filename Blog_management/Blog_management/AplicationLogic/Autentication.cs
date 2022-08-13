f76ef4a76647f671dc80bee2e438c0eb3fd21a91
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 17:37:56 +0400

    GetName method added Autentication class

--- Blog_management/Blog_management/AplicationLogic/Autentication.cs
+++ Blog_management/Blog_management/AplicationLogic/Autentication.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog_management.AplicationLogic.Validations;
+using Blog_management.DataBase.Model;
+using Blog_management.DataBase.Reposity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace Blog_management.AplicationLogic
 {
-    internal class Autentication
+    public class Autentication
     {
+        public static string GetName()
+        {
+            Console.Write("Please enter user's name : ");
+            string firstName = Console.ReadLine();
+            while (!UserValidations.IsValidFirstName(firstName))
+            {
+                Console.Write("Please enter correct user's name : ");
+                firstName = Console.ReadLine();
+            }
+            return firstName;
+        }
     }
 }
