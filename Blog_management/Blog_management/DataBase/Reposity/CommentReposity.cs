101a82cf20223c659908673109b376e4a033a97a
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 16:13:25 +0400

    Default comments added CommentReposity class and complated

--- Blog_management/Blog_management/DataBase/Reposity/CommentReposity.cs
+++ Blog_management/Blog_management/DataBase/Reposity/CommentReposity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,30 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Reposity
 {
-    
+    public class CommentReposity : Common.Reposity<BlogComments, int>
+    {
+        public static BlogReposity blogRepo = new BlogReposity();
+        static CommentReposity()
+        {
+
+            DBcontect.Add(new BlogComments(blogRepo.GetById("BL11111"), UserReposity.GetUerByEmail("ayroali@gmail.com"), "Beli beli tamamile raziyam"));
+            DBcontect.Add(new BlogComments(blogRepo.GetById("BL11111"), UserReposity.GetUerByEmail("haci@gmail.com"), "he eledi"));
+            DBcontect.Add(new BlogComments(blogRepo.GetById("3"), UserReposity.GetUerByEmail("haci@gmail.com"), "iajfhkkafhfskj e"));
+
+
+        }
+
+        //public static void DeleteComment(Blog blog)
+        //{
+        //    foreach(BlogComments comment in DBcontect)
+        //    {
+        //        if(comment.WhichBlog==blog)
+        //        {
+        //            DBcontect.Remove(comment);
+        //        }
+        //    }
+        //}
+
+
+    }
 }
