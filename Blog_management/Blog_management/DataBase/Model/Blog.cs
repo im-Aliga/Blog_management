56170048d589ee2e5a3b8eaaed3d38b87783ad19
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 14:57:46 +0400

    Blog class has been completed

--- Blog_management/Blog_management/DataBase/Model/Blog.cs
+++ Blog_management/Blog_management/DataBase/Model/Blog.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model.Common;
+using Blog_management.DataBase.Reposity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Model
 {
-    internal class Blog
+    public class Blog : Entity<string>
     {
+        public User Owner { get; set; }
+
+        public string BlogTitle { get; set; }
+
+        public string BlogContent { get; set; }
+
+        public BlogStatus BlogStatus { get; set; }
+
+        public Blog(User owner, string blogTitle, string blogContent, BlogStatus blogStatus, string id = null)
+        {
+            Owner = owner;
+            BlogTitle = blogTitle;
+            BlogContent = blogContent;
+            BlogStatus = blogStatus;
+            if (id != null)
+            {
+                Id = id;
+            }
+            else
+            {
+                Id = BlogReposity.BlogCode;
+            }
+
+        }
+
+        public string BlogInfo()
+        {
+            return RegistrationTime.ToString("dd/MM/yyyy") + " " + Id + " " + Owner.FirstName + " " + Owner.LastName + "\n" + "===" + BlogTitle + "===" + " \n" + BlogContent + "\n";
+        }
     }
 }
