90e422fe77a655de16dc6a1e15d60f2c99d374e1
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 17:14:32 +0400

    IsValidTitle method added BlogValidations class

--- Blog_management/Blog_management/AplicationLogic/Validations/BlogValidations.cs
+++ Blog_management/Blog_management/AplicationLogic/Validations/BlogValidations.cs
@@ -8,5 +8,15 @@ namespace Blog_management.AplicationLogic.Validations
 {
     internal class BlogValidations
     {
+        public static bool IsValidTitle(string title)
+        {
+            if (title.Length >= 10 & title.Length <= 35)
+            {
+                return true;
+
+            }
+            Console.WriteLine("Title length sould be between 10 and 35");
+            return false;
+        }
     }
 }
