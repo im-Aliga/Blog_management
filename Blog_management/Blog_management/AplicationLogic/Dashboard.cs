d9bfa8888c53ade40caad0514790463b8a8e0f1a
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 18:17:11 +0400

    AdminPanel method added Dashboard class

--- Blog_management/Blog_management/AplicationLogic/Dashboard.cs
+++ Blog_management/Blog_management/AplicationLogic/Dashboard.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model;
+using Blog_management.DataBase.Reposity;
+using Blog_management.DataBase.Reposity.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace Blog_management.AplicationLogic
 {
-    internal class Dashboard
+    public static partial class Dashboard
     {
+        public static User CurrentUser { get; set; }
+
+        public static void AdminPAnel()
+        {
+            Reposity<User, int> baserepo = new Reposity<User, int>();
+
+            BlogReposity blogRepo = new BlogReposity();
+
+            InboxReposity inboxRepo = new InboxReposity();
+
+            Admin admin1 = (Admin)UserReposity.GetUerByEmail(CurrentUser.Email);
+
+            Console.WriteLine(admin1.GetInfo());
+        }
+
     }
 }
