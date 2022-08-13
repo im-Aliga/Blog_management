f2c1d6ec89660d875c5b5d4a93f7cfaf209e3402
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 15:05:33 +0400

    BlogStatus enum has been complated

--- Blog_management/Blog_management/DataBase/Model/BlogStatus.cs
+++ Blog_management/Blog_management/DataBase/Model/BlogStatus.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Model
 {
-    internal class BlogStatus
+    public enum BlogStatus
     {
+        Created = 1,
+        Approve = 2,
+        Reject = 3,
+
     }
 }
