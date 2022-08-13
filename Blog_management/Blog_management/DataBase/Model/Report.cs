8b7b01d31e5a5f6073677beb3d6a0f3cbf3dd14b
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 15:09:54 +0400

    Report class has been complated

--- Blog_management/Blog_management/DataBase/Model/Report.cs
+++ Blog_management/Blog_management/DataBase/Model/Report.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Model
 {
-    internal class Report
+    internal class Report : Entity<int>
     {
+        public string Content { get; set; }
+
+        public User From { get; set; }
+
+        public User To { get; set; }
+
+        public DateTime ReportDate { get; set; } = DateTime.Now;
+
+        public Report(string content, User from, User to, int? id = null)
+        {
+            Content = content;
+            From = from;
+            To = to;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else
+            {
+                Id = UserReposity.IdCounter;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"From : {From.Email}, To : {To.Email}, Content : {Content},Is Admin{To is Admin}, Report date: {ReportDate.ToString("HH:mm dd.MM.yyyy")}";
+        }
     }
 }
