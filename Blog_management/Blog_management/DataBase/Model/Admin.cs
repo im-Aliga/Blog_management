79b94077b688e3021c63f6dd8172263daf389fad
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 14:54:26 +0400

    Admin class has been complated

--- Blog_management/Blog_management/DataBase/Model/Admin.cs
+++ Blog_management/Blog_management/DataBase/Model/Admin.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Model
 {
-    internal class Admin
+    public class Admin : User
     {
+        public Admin(string firstName, string lastName, string email, string password, int id)
+            : base(firstName, lastName, email, password, id)
+        {
+
+        }
+
+        public Admin(string firstName, string lastname, string email, string password)
+            : base(firstName, lastname, email, password)
+        {
+
+        }
+
+        public Admin(string firstName, string lastname)
+            : base(firstName, lastname)
+        {
+
+        }
+
+        public override string GetInfo()
+        {
+            return $"Hello Admin: {Id}:{FirstName}:{LastName}:{RegistrationTime}";
+        }
     }
 }
