482c29a97e525da67d45de8a13796a2e126ec9e3
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 15:16:27 +0400

    Add method added Reposity class

--- Blog_management/Blog_management/DataBase/Reposity/Common/Reposity.cs
+++ Blog_management/Blog_management/DataBase/Reposity/Common/Reposity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_management.DataBase.Model.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Blog_management.DataBase.Reposity.Common
 {
-    internal class Reposity
+    public  class Reposity <TEntity,Tid>
+        where TEntity : Entity<Tid>
     {
+        protected static List<TEntity> DBcontect { get; set; } = new List<TEntity>();
+
+        public TEntity Add(TEntity entry)
+        {
+            DBcontect.Add(entry);
+            return entry;
+        }
     }
 }
