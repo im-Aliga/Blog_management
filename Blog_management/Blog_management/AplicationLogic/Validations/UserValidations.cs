5c40ff80ddd1f63956834e59aef8ef8eb232190c
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-08-13 17:23:46 +0400

    IsVAlidFirstName method added UserValidations class

--- Blog_management/Blog_management/AplicationLogic/Validations/UserValidations.cs
+++ Blog_management/Blog_management/AplicationLogic/Validations/UserValidations.cs
@@ -8,5 +8,18 @@ namespace Blog_management.AplicationLogic.Validations
 {
     internal class UserValidations
     {
+        public static bool IsValidFirstName(string firstName)
+        {
+            Regex regex = new Regex(@"^(?=[A-Z]{1})([A-Za-z]{3,30})$");
+
+            if (regex.IsMatch(firstName))
+            {
+                return true;
+            }
+
+            Console.WriteLine("The name you entered is incorrect, make sure the name contains only letters, the first letter is capitalized, and the length is greater than 3 and less than 30.");
+
+            return false;
+        }
     }
 }
